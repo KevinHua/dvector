7d8fa32998d5f4be842adb1aaf74ddacf326bf89
Hua Hongliang <huahl@126.com>
2020-11-26 18:36:03 +0800

    scripts

--- run_prepare.sh
+++ run_prepare.sh
@@ -5,4 +5,5 @@ OUT_DIR="/home/featurize/data/train_data/melsp"
 CFG_FILE="./toolkit_config.yaml"
 INPUT_DIRS="/home/featurize/data/zhthchs30 /home/featurize/data/zhspeechocean"
 
-python prepare.py -m $MAX_AMOUNT -s $OUT_DIR -c $CFG_FILE $INPUT_DIRS
+#python prepare.py -m $MAX_AMOUNT -s $OUT_DIR -c $CFG_FILE $INPUT_DIRS
+python prepare.py -s $OUT_DIR -c $CFG_FILE $INPUT_DIRS
--- run_train.sh
+++ run_train.sh
@@ -5,4 +5,5 @@ MODEL_DIR="/home/featurize/data/train_data/dvector_models"
 CONFIG_PATH=dvector_config.yaml
 BATCH_SIZE=8
 
+CHECKPOINT_PATH="/home/featurize/data/train_data/dvector_models/ckpt-120000.tar"
 python train.py --n_speakers=$BATCH_SIZE $DATA_DIR $MODEL_DIR dvector_config.yaml
